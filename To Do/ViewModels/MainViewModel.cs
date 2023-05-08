8393ee1d7a00183b05d98e7ff2b9d30b2e52212e
zzhxufeng <zzhxufeng@163.com>
2023-05-08 14:11:42 +0800

    Main: title bar. - icon window - min window - max window - close window - window round corder

--- To Do/ViewModels/MainViewModel.cs
+++ To Do/ViewModels/MainViewModel.cs
@@ -1,8 +1,41 @@
+using Prism.Commands;
 using Prism.Mvvm;
+using System.Windows;
 
 namespace To_Do.Services;
 
 public class MainViewModel : BindableBase
 {
+	private Visibility visibility;
 
+	public Visibility Visibility
+    {
+		get { return visibility; }
+		set { visibility = value; RaisePropertyChanged(); }
+	}
+
+	private DelegateCommand openDetailCommand;
+
+	public DelegateCommand OpenDetailCommand
+	{
+		get { return openDetailCommand; }
+		set { openDetailCommand = value; RaisePropertyChanged(); }
+	}
+
+	public MainViewModel()
+	{
+		openDetailCommand = new DelegateCommand(OpenDetail);
+	}
+
+    private void OpenDetail()
+    {
+		if (Visibility == Visibility.Collapsed)
+		{
+			Visibility = Visibility.Visible;
+		}
+		else
+		{
+            Visibility = Visibility.Collapsed;
+        }
+    }
 }
--- To Do/Views/MainView.xaml.cs
+++ To Do/Views/MainView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace To_Do;
 
@@ -7,5 +8,29 @@ public partial class MainView : Window
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
+    }
+
+    private void btnMin_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void btnMax_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState == WindowState.Maximized ?
+            WindowState.Normal : WindowState.Maximized;
+    }
+
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
