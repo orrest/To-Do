a31ea234a658b85cbd46cb853a30075da706cc49
xufeng <zzhxufeng@163.com>
2023-05-11 22:00:09 +0800

    Main: menu.

---
+++ To Do/Models/Menu.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace To_Do.Models;
+
+public class MenuItem
+{
+    public string Icon { get; set; }
+    public string Color { get; set; }
+    public string Title { get; set; }
+}
--- To Do/ViewModels/MainViewModel.cs
+++ To Do/ViewModels/MainViewModel.cs
@@ -1,6 +1,9 @@
 using Prism.Commands;
 using Prism.Mvvm;
+using System;
+using System.Collections.ObjectModel;
 using System.Windows;
+using To_Do.Models;
 
 namespace To_Do.Services;
 
@@ -14,17 +17,18 @@ public class MainViewModel : BindableBase
 		set { visibility = value; RaisePropertyChanged(); }
 	}
 
-	private DelegateCommand openDetailCommand;
+    public DelegateCommand OpenDetailCommand { get; private set; }
 
-	public DelegateCommand OpenDetailCommand
-	{
-		get { return openDetailCommand; }
-		set { openDetailCommand = value; RaisePropertyChanged(); }
-	}
+    public DelegateCommand NavigationCommand { get; private set; }
+
+    public ObservableCollection<MenuItem> MenuItems { get; private set; }
+		= new ObservableCollection<MenuItem>();
 
 	public MainViewModel()
 	{
-		openDetailCommand = new DelegateCommand(OpenDetail);
+		OpenDetailCommand = new DelegateCommand(OpenDetail);
+        NavigationCommand = new DelegateCommand(Naivgate);
+        InitMenuItems();
 	}
 
     private void OpenDetail()
@@ -38,4 +42,50 @@ public class MainViewModel : BindableBase
             Visibility = Visibility.Collapsed;
         }
     }
+
+    private void Naivgate()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void InitMenuItems()
+    {
+        MenuItems.Add(new MenuItem
+        {
+            Icon = "ExclamationThick",
+            Color = "#7b8791",
+            Title = "重要",
+        });
+        MenuItems.Add(new MenuItem
+        {
+            Icon = "CalendarTodayOutline",
+            Color = "#a9828a",
+            Title = "日任务",
+        });
+        MenuItems.Add(new MenuItem
+        {
+            Icon = "CalendarWeekOutline",
+            Color = "#78b1ad",
+            Title = "周任务",
+        });
+        MenuItems.Add(new MenuItem
+        {
+            Icon = "CalendarMonthOutline",
+            Color = "#84b09d",
+            Title = "月任务",
+        });
+        MenuItems.Add(new MenuItem
+        {
+            Icon = "CalendarCheckOutline",
+            Color = "#bac8d4",
+            Title = "长期任务",
+        });
+        MenuItems.Add(new MenuItem
+        {
+            Icon = "EmailArrowLeftOutline",
+            Color = "#9b2a46",
+            Title = "电子邮件",
+        });
+    }
+
 }
