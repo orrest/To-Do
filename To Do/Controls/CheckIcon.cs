cecc1c28e795c886e5c7a8c3c775ceef5a7c1f28
xufeng <zzhxufeng@163.com>
2023-05-25 23:57:08 +0800

    DayView: customize user control (bug)

---
+++ To Do/Controls/CheckIcon.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace To_Do.Controls
+{
+    public class CheckIcon : Control
+    {
+        static CheckIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckIcon), new FrameworkPropertyMetadata(typeof(CheckIcon)));
+        }
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsChecked.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked", typeof(bool), typeof(CheckIcon), new PropertyMetadata(false));
+
+
+    }
+}
---
+++ To Do/Controls/IndicationCheckIcon.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace To_Do.Controls
+{
+    public class IndicationCheckIcon : Control
+    {
+        static IndicationCheckIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(IndicationCheckIcon), new FrameworkPropertyMetadata(typeof(IndicationCheckIcon)));
+        }
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsChecked.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked", typeof(bool), typeof(IndicationCheckIcon), new PropertyMetadata(false));
+
+
+    }
+}
