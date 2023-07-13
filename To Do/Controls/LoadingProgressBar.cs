7211a43b2f1700a34959e8644fa386a586d2f268
xufeng <zzhxufeng@163.com>
2023-07-13 20:57:46 +0800

    UI LoadingProgressBar

---
+++ To Do/Controls/LoadingProgressBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace To_Do.Controls;
+
+public class LoadingProgressBar : Control
+{
+    static LoadingProgressBar()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(LoadingProgressBar),
+            new FrameworkPropertyMetadata(typeof(LoadingProgressBar)));
+    }
+
+    public bool IsLoading
+    {
+        get { return (bool)GetValue(IsLoadingProperty); }
+        set { SetValue(IsLoadingProperty, value); }
+    }
+
+    public static readonly DependencyProperty IsLoadingProperty =
+        DependencyProperty.Register(
+            "IsLoading",
+            typeof(bool),
+            typeof(LoadingProgressBar),
+            new PropertyMetadata(false));
+
+
+}
